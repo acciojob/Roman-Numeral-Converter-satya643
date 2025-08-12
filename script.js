7992550e5f9e960c4467b3e9e07d692a9a0f0724
Satya Prakash <143164106+satya643@users.noreply.github.com>
2025-08-12 23:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  // Combined table with subtractive cases in correct order
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let result = '';
 
-  // Extended pairs for subtractive cases
-  const subtractives = {
-    900: 'CM',
-    400: 'CD',
-    90: 'XC',
-    40: 'XL',
-    9: 'IX',
-    4: 'IV'
-  };
-
-  // First handle subtractive values
-  for (let value of [900, 400, 90, 40, 9, 4]) {
-    while (num >= value) {
-      result += subtractives[value];
-      num -= value;
-    }
-  }
-
-  // Then handle standard values from obj
-  for (let i = 0; i <= 6; i++) {
-    let symbol = obj[i][0];
-    let value = obj[i][1];
+  for (let [symbol, value] of romanMap) {
     while (num >= value) {
       result += symbol;
       num -= value;
@@ -41,9 +27,6 @@ function convertToRoman(num) {
 
   return result;
 }
-
-// Example test
-// console.log(convertToRoman(36)); // XXXVI
 
 // do not edit below this line
 module.exports = convertToRoman;
